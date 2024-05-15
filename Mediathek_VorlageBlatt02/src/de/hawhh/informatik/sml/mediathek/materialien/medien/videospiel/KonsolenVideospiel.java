74bc969b61a1def027c3fd342bd3c6de4d02eeec
minmyat <minmyatthu3644@gmail.com>
2024-05-15 18:12:43 +0200

    packaging a few classes

---
+++ Mediathek_VorlageBlatt02/src/de/hawhh/informatik/sml/mediathek/materialien/medien/videospiel/KonsolenVideospiel.java
@@ -0,0 +1,26 @@
+package de.hawhh.informatik.sml.mediathek.materialien.medien;
+
+public class KonsolenVideospiel extends AbstractVideospiel
+{
+    /**
+     * Initialisiert ein neues KonsolenVideospiel.
+     *
+     * @param titel     Der Titel des Spiels
+     * @param kommentar Ein Kommentar zum Spiel
+     * @param system    Die Bezeichnung des Systems
+     * @require titel != null
+     * @require kommentar != null
+     * @require system != null
+     * @ensure getTitel() == titel
+     * @ensure getKommentar() == kommentar
+     * @ensure getSystem() == system
+     */
+    public KonsolenVideospiel(String titel, String kommentar, String system) {
+        super(titel, kommentar, system);
+    }
+
+    public int getPreisNachTagen(int tage) {
+        int counter = tage / 3;
+        return 700 * counter;
+    }
+}
---
+++ Mediathek_VorlageBlatt02/src/de/hawhh/informatik/sml/mediathek/materialien/medien/videospiel/KonsolenVideospielTest.java
@@ -0,0 +1,53 @@
+package de.hawhh.informatik.sml.mediathek.materialien.medien.tests;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import de.hawhh.informatik.sml.mediathek.materialien.medien.KonsolenVideospiel;
+
+/**
+ */
+public class KonsolenVideospielTest extends AbstractVideospielTest
+{
+    private static final String SYSTEM = "Konsole";
+    private final KonsolenVideospiel _videospiel = (KonsolenVideospiel) super.getExamplar();
+
+    @Override
+    protected KonsolenVideospiel getMedium() {
+        return new KonsolenVideospiel(TITEL, KOMMENTAR, SYSTEM);
+    }
+
+    @Test
+    public void testKonstruktor() {
+        assertEquals(SYSTEM, _videospiel.getSystem());
+    }
+
+    @Test
+    public void testGetSystem()
+    {
+        assertEquals(SYSTEM, _videospiel.getSystem());
+    }
+
+    @Test
+    public void testSetSystem()
+    {
+        KonsolenVideospiel medium = getMedium();
+        medium.setSystem("System2");
+        assertEquals(medium.getSystem(), "System2");
+    }
+
+    @Test
+    public void testBerechneMietgebuehr()
+    {
+        assertEquals("Falscher EuroAnteil",4, _videospiel.berechneMietgebuehr(2).getEuroAnteil());
+        assertEquals("Falscher EuroAnteil",13, _videospiel.berechneMietgebuehr(3).getEuroAnteil());
+        assertEquals("Falscher EuroAnteil",17, _videospiel.berechneMietgebuehr(5).getEuroAnteil());
+        assertEquals("Falscher EuroAnteil",28, _videospiel.berechneMietgebuehr(7).getEuroAnteil());
+        assertEquals("Falscher EuroAnteil",95, _videospiel.berechneMietgebuehr(23).getEuroAnteil());
+
+        assertEquals("Falscher CentAnteil",0, _videospiel.berechneMietgebuehr(1).getCentAnteil());
+
+    }
+
+}
